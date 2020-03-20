8032ee043e6a594b01f29cd94841a0beb5aa2633
bbqqpp <shmily_lms@qq.com>
2020-03-20 14:18:04 +0800

    first commit change

--- src/firstPackage/HelloWorld.java
+++ src/firstPackage/HelloWorld.java
@@ -3,5 +3,6 @@ package firstPackage;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloWorld");
+        System.out.println("testChange");
     }
 }
